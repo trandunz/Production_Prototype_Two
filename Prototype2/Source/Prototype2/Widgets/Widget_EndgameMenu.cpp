d98c61f7e1c9c036374aadb5f5f29799c090f0b9
OMaloso <88115296+OMaloso@users.noreply.github.com>
2023-03-17 19:20:48 +1300

    Starting Endgame menu. Not complete yet.

---
+++ Prototype2/Source/Prototype2/Widgets/Widget_EndgameMenu.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget_EndgameMenu.h"
+
+#include "Components/TextBlock.h"
+#include "Kismet/GameplayStatics.h"
+#include "Prototype2/Gamestates/Prototype2Gamestate.h"
+#include "Prototype2/Prototype2PlayerState.h"
+
+void UWidget_EndgameMenu::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (auto* gameState = Cast<APrototype2Gamestate>(UGameplayStatics::GetGameState(GetWorld())))
+	{
+		GameStateRef = gameState;
+	}
+}
+
+void UWidget_EndgameMenu::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+
+}
+
+void UWidget_EndgameMenu::UpdateWinnerText()
+{
+	if (GameStateRef)
+	{
+		// Set first player as winner
+		Winner = 0;
+		if (auto player = GameStateRef->PlayerArray[0])
+		{
+			if (auto* playerState = Cast<APrototype2PlayerState>(player))
+			{
+				WinnerPoints = playerState->Coins;
+			}
+		}
+
+		// Check remaining players
+		for (int i = 1; i < GameStateRef->PlayerArray.Num(); i++)
+		{
+			if (auto player = GameStateRef->PlayerArray[i])
+			{
+				if (auto* playerState = Cast<APrototype2PlayerState>(player))
+				{
+					if (playerState->Coins > WinnerPoints)
+					{
+						Winner = i;
+						WinnerPoints = playerState->Coins;
+					}
+				}
+			}
+		}
+	}
+
+	// Set text of who won
+	if (Winner == 0)
+	{
+		GameWinnerText->SetText(FText::FromString("Player 1 Wins"));
+	}
+	else if (Winner == 1)
+	{
+		GameWinnerText->SetText(FText::FromString("Player 2 Wins"));
+	}
+	else if (Winner == 2)
+	{
+		GameWinnerText->SetText(FText::FromString("Player 3 Wins"));
+	}
+	else
+	{
+		GameWinnerText->SetText(FText::FromString("Player 4 Wins"));
+	}
+	
+	// Update points
+	GameWinnerPoints->SetText(FText::FromString(FString::FromInt(WinnerPoints)));
+
+	
+}
