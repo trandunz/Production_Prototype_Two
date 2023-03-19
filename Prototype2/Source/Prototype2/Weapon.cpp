6dbbd133668290fee1ce8f002f44c97abc8dfc41
grovesben <benjaminjames.b@gmail.com>
2023-03-20 12:44:43 +1300

    Attack animation montage connected, weapon sockets in

--- Prototype2/Source/Prototype2/Weapon.cpp
+++ Prototype2/Source/Prototype2/Weapon.cpp
@@ -2,10 +2,30 @@
 
 
 #include "Weapon.h"
+
+#include "EdGraphSchema_K2_Actions.h"
 #include "Prototype2Character.h"
 void AWeapon::Interact(APrototype2Character* player)
 {
-	ItemComponent->Interact(player, this);
+	//ItemComponent->Interact(player, this);
+
+	/* To move to ItemComponent - move the EInterfaceType to ItemComponent too */
+	
+	// Setup Mesh
+	ItemComponent->Mesh->SetSimulatePhysics(false);
+	ItemComponent->Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+	// Set players HeldItem
+	player->Weapon = this;
+	
+	// Attach to socket
+	this->AttachToComponent(player->GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, FName("WeaponHolsterSocket"));
+	
+	// Assign Players HeldItem
+	player->Weapon = this;
+
+	// Debug
+	UE_LOG(LogTemp, Warning, TEXT("Weapon attached to WeaponHolsterSocket"));	
 }
 
 AWeapon::AWeapon()
