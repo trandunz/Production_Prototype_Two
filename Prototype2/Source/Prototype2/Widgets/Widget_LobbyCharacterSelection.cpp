3376180f8fe919353c78ee07b77bebec0b8d97cb
OMaloso <88115296+OMaloso@users.noreply.github.com>
2023-06-03 14:00:58 +1200

    HUD images for character change - only changes UI
    Only changes the UI images, need help from Will to get the character changing as requires networking
    Updated button sounds on lobby

---
+++ Prototype2/Source/Prototype2/Widgets/Widget_LobbyCharacterSelection.cpp
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget_LobbyCharacterSelection.h"
+
+#include "Components/Image.h"
+#include "Kismet/GameplayStatics.h"
+#include "Prototype2/Gamestates/Prototype2Gamestate.h"
+
+void UWidget_LobbyCharacterSelection::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	PlayerImage->SetBrushFromTexture(Texture_CowRed);
+	PlayerColourImage->SetColorAndOpacity(FLinearColor(0.427083f, 0.100098f, 0.100098f, 1.0f));
+}
+
+void UWidget_LobbyCharacterSelection::ChangeCharacterLeft()
+{
+}
+
+void UWidget_LobbyCharacterSelection::ChangeCharacterRight()
+{
+}
+
+void UWidget_LobbyCharacterSelection::ChangeCharacterColourLeft()
+{
+	switch(CharacterColour)
+	{
+	case ECharacterColours::RED:
+		CharacterColour = ECharacterColours::YELLOW;
+		break;
+	case ECharacterColours::BLUE:
+		CharacterColour = ECharacterColours::RED;
+		break;
+	case ECharacterColours::GREEN:
+		CharacterColour = ECharacterColours::BLUE;
+		break;
+	case ECharacterColours::YELLOW:
+		CharacterColour = ECharacterColours::GREEN;
+		break;
+	default:
+		CharacterColour = ECharacterColours::RED;
+		break;
+	}
+
+	UpdateCharacterImage();
+}
+
+void UWidget_LobbyCharacterSelection::ChangeCharacterColourRight()
+{
+	switch(CharacterColour)
+	{
+	case ECharacterColours::RED:
+		CharacterColour = ECharacterColours::BLUE;
+		break;
+	case ECharacterColours::BLUE:
+		CharacterColour = ECharacterColours::GREEN;
+		break;
+	case ECharacterColours::GREEN:
+		CharacterColour = ECharacterColours::YELLOW;
+		break;
+	case ECharacterColours::YELLOW:
+		CharacterColour = ECharacterColours::RED;
+		break;
+	default:
+		CharacterColour = ECharacterColours::RED;
+		break;
+	}
+
+	UpdateCharacterImage();
+}
+
+void UWidget_LobbyCharacterSelection::UpdateCharacterImage()
+{
+	switch(CharacterColour)
+	{
+	case ECharacterColours::RED:
+		PlayerImage->SetBrushFromTexture(Texture_CowRed);
+		PlayerColourImage->SetColorAndOpacity(FLinearColor(0.427083f, 0.100098f, 0.100098f, 1.0f));
+		break;
+	case ECharacterColours::BLUE:
+		PlayerImage->SetBrushFromTexture(Texture_CowBlue);
+		PlayerColourImage->SetColorAndOpacity(FLinearColor(0.034125f, 0.118703f, 0.192708f, 1.0f));
+		break;
+	case ECharacterColours::GREEN:
+		PlayerImage->SetBrushFromTexture(Texture_CowGreen);
+		PlayerColourImage->SetColorAndOpacity(FLinearColor(0.078957f, 0.192708f, 0.056207f, 1.0f));
+		break;
+	case ECharacterColours::YELLOW:
+		PlayerImage->SetBrushFromTexture(Texture_CowYellow);
+		PlayerColourImage->SetColorAndOpacity(FLinearColor(0.317708f, 0.180145, 0.033095, 1.0f));
+		break;
+	default:
+		PlayerImage->SetBrushFromTexture(Texture_CowRed);
+		PlayerColourImage->SetColorAndOpacity(FLinearColor(0.427083f, 0.100098f, 0.100098f, 1.0f));
+		break;
+	}
+}
---
+++ Prototype2/Source/Prototype2/Widgets/Widget_LobbyCharacterSelection.h
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Widget_LobbyCharacterSelection.generated.h"
+
+/**
+ * 
+ */
+
+UENUM(BlueprintType)
+enum class ECharacters : uint8
+{
+	COW,
+	PIG,
+	CHICKEN,
+	DUCK
+};
+
+UENUM(BlueprintType)
+enum class ECharacterColours : uint8
+{
+	RED,
+	BLUE,
+	GREEN,
+	YELLOW
+};
+
+UCLASS()
+class PROTOTYPE2_API UWidget_LobbyCharacterSelection : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	class APrototype2Gamestate* GameStateRef;
+
+	
+	UPROPERTY(VisibleAnywhere, meta=(BindWidget))
+	class UImage* PlayerImage;
+
+	UPROPERTY(VisibleAnywhere, meta=(BindWidget))
+	class UImage* PlayerColourImage;
+
+	// -- Character image textures --
+	// Cow
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* Texture_CowRed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* Texture_CowBlue;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* Texture_CowGreen;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* Texture_CowYellow;
+	
+	// Character & Colour
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ECharacters Character{ECharacters::COW};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ECharacterColours CharacterColour{ECharacterColours::RED};
+
+	// Functions
+
+	virtual void NativeOnInitialized() override;
+
+	UFUNCTION(BlueprintCallable)
+	void ChangeCharacterLeft();
+
+	UFUNCTION(BlueprintCallable)
+	void ChangeCharacterRight();
+
+	UFUNCTION(BlueprintCallable)
+	void ChangeCharacterColourLeft();
+
+	UFUNCTION(BlueprintCallable)
+	void ChangeCharacterColourRight();
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateCharacterImage();
+	
+};
