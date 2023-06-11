ab2c25cc6984b986762e76052cdb6b06c6ad5193
DawnB9999 <dawn06737@gmail.com>
2023-06-12 10:43:36 +1200

    seperated weapon and normal seed spawning

---
+++ Prototype2/Source/Prototype2/PlantSeed.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Seed.h"
+#include "PlantSeed.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPE2_API APlantSeed : public ASeed
+{
+	GENERATED_BODY()
+	
+};
---
+++ Prototype2/Source/Prototype2/WeaponSeed.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Seed.h"
+#include "WeaponSeed.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPE2_API AWeaponSeed : public ASeed
+{
+	GENERATED_BODY()
+	
+};
