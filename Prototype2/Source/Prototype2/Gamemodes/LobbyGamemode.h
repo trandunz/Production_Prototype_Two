23cf964552045fbf81254dddde0d370066a735e6
OMaloso <88115296+OMaloso@users.noreply.github.com>
2023-06-12 12:44:33 +1200

    Character lobby positions

--- Prototype2/Source/Prototype2/Gamemodes/LobbyGamemode.h
+++ Prototype2/Source/Prototype2/Gamemodes/LobbyGamemode.h
@@ -19,4 +19,14 @@ public:
 	
 	UPROPERTY(EditAnywhere)
 	TArray<UMaterialInstance*> PlayerMaterials{{},{},{},{}};
+
+
+	// Player character lobby positions
+	FVector Position1{567.4f,-143.0f,97.0f}; // Left position for 4 player
+	FVector Position2{446.0f, 187.2f, 97.0f}; // Left position for 3 player
+	FVector Position3{324.6f,-231.4f,97.0f}; // Left position for 2 player
+	FVector Position4{212.2f, -272.3f, 97.0f}; // Centre position for 3 player
+	FVector Position5{99.8f,-313.2f,97.0f}; // // Right position for 2 player
+	FVector Position6{-11.5, -353.7f, 97.0f}; // Right position for 3 player
+	FVector Position7{-122.7f,-394.2f,97.0f}; // Right position for 4 player
 };
